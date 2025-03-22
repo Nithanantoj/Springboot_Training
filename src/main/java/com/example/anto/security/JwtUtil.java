4203bdcfb496ed17f4baa7aa795a651d861f4997
Nithanantoj <nithananto.j2022cse@sece.ac.in>
2025-03-22 10:17:04 +0530

    Jwt Authentication

--- src/main/java/com/example/anto/security/JwtUtil.java
+++ src/main/java/com/example/anto/security/JwtUtil.java
@@ -19,7 +19,7 @@ import java.util.function.Function;
 public class JwtUtil {
 
     private SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256); // Change this to a secure key
-    private long validity = 20 * 1000; // 5 minutes
+    private long validity = 5 * 60 * 1000; // 5 minutes
 
 
 
