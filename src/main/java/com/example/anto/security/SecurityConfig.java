354eca86bd53e64a60be4444cfa1f87392222832
Nithanantoj <nithananto.j2022cse@sece.ac.in>
2025-03-27 14:54:50 +0530

    cors configure update

--- src/main/java/com/example/anto/security/SecurityConfig.java
+++ src/main/java/com/example/anto/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // Frontend origin
+        config.setAllowedOrigins(List.of("http://localhost:5173")); // Frontend origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);    // For cookies and credentials
